c9f7123ab26eeb852842ecfa4f85a184a815df5c
ismailahmadi <49345569+ismailahmadi@users.noreply.github.com>
2019-06-28 10:42:42 -0400

    Update server.py

--- app/server.py
+++ app/server.py
@@ -9,7 +9,7 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/preview/export.pkl?role=personal'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1ZGiNumaKqVVUyKGqiHc4Vcr7QamyES4h'
 export_file_name = 'export.pkl'
 
 classes = ['tennis', 'badminton']
